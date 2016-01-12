08f1947d0592d48bc9c5b53527bac54fe7847d25
Joe Pavitt <joepavit@uk.ibm.com>
2016-01-12 10:25:03 +0000

    Update Express Server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
+'use strict';
+
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // require
 require('./routes')(app);
 
-app.use(express.static('client'));
+// static - all our js, css, images, etc go into the assets path
+app.use('/app', express.static(path.join(__dirname, '../client', 'app')));
+app.use('/bower_components', express.static(path.join(__dirname, '../client', 'bower_components')));
+app.use('/style', express.static(path.join(__dirname, '../client', 'style')));
+app.use('/assets', express.static(path.join(__dirname, '../client', 'assets')));
+
+// This route deals enables HTML5Mode by forwarding missing files to the index.html
+app.all('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 var server = app.listen(3000, function () {
 
